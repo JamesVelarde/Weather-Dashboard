d232a331d586c11d449f93e573822f683d1dfcd7
James <jimmyvelarde3302@gmail.com>
2022-01-31 14:37:29 -0700

    weather dashboard updated

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,156 @@
-// OpenWeather API key f155825c2d9975029ce899c709c33acf// OpenWeather API key f155825c2d9975029ce899c709c33acf
+const currentDayCard = document.querySelector('.today-weather');
+const searchHistoryEl = document.querySelector('.search-history');
+const searchBtn = document.querySelector('#search-typed-city');
+const apiKey = '95d906b07f593f4392e9dcc32de64324'
 
-// Select elements
-var iconElement = document.querySelector(".weather-icon");
-var tempElement = document.querySelector(".temperature-value p");
-var descElement = document.querySelector(".temperature-description p");
-var locationElement = document.querySelector(".location p");
-var notificationElement = document.querySelector(".notification");
+const lastSearchCity = localStorage.getItem('lastCity') ? localStorage.getItem('lastCity') : 'Atlanta';
 
-// App data
-var weather = {};
+let currCity = {};
+let currDay = moment().format('M/D/YY');
+let weatherData;
+let forecastData;
+let searchHistory = localStorage.getItem('searchHistory');
 
-weather.temperature = {
-    unit : "fahrenheit"
+getCurrentWeather(lastSearchCity);
+
+
+function setCurrentCity(city) {
+    const currentCityEl = document.querySelector('#current-city');
+    currDay = moment().format('M/D/YY');
+    currentCityEl.textContent = city + " " + currDay;
+};
+
+function updateCurrentWeatherCard(city) {
+    currCity.name = city;
+    currCity.lat = weatherData.coord.lat;
+    currCity.lon = weatherData.coord.lon;
+
+    setCurrentCity(city);
+    document.querySelector('#current-temp').textContent = Math.floor(weatherData.main.temp);
+    document.querySelector('#current-wind').textContent = weatherData.wind.speed;
+    document.querySelector('#current-humidity').textContent = Math.floor(weatherData.main.humidity);
+    document.querySelector('#today-icon').setAttribute("src", getWeatherIcon(weatherData));
+    handlehistoricSearches(city);
+    getFiveDayForecast();
 }
 
-var key = "f155825c2d9975029ce899c709c33acf";
+function getWeatherIcon(dayData) {
+    return "http://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+}
 
-// Check if browser supports Geolocation
-if("geolocation" in navigator){
-    navigator.geolocation.getCurrentPosition(setPosition, showError);
+function updateFiveDayForecast() {
+    handleUvColor();
+    for(var i = 1; i <= 5; i++) {
+        updateFutureDayCard(i, forecastData.daily[i]);
     }
-    else{
-        notificationElement.style.display = "block";
-        notificationElement.innerHTML = "<p>Browser doesn't support Geolocation</p>";
+}
+
+function updateFutureDayCard(index, dayForecast) {
+    var thisDayCard = document.querySelector('#day-' + index + '-card');
+    thisDayCard.querySelector(".card-title").textContent = moment.unix(dayForecast.dt).format('M/D/YY');
+    thisDayCard.querySelector(".temp-section").textContent = Math.floor(dayForecast.temp.day);
+    thisDayCard.querySelector(".wind-section").textContent = dayForecast.wind_speed;
+    thisDayCard.querySelector(".humidity-section").textContent = dayForecast.humidity;
+    thisDayCard.querySelector(".weather-icon").setAttribute("src", getWeatherIcon(dayForecast));
+}
+
+function handleUvColor() {
+    const uvButton = document.querySelector('#current-uv-index');
+    uvButton.textContent = forecastData.current.uvi;
+    uvButton.classList.remove('btn-success', 'btn-danger', 'btn-warning');
+    if (forecastData.current.uvi < 3) {
+        uvButton.classList.add('btn-success');
+    } else if (forecastData.current.uvi > 3 && forecastData.current.uvi < 6) {
+        uvButton.classList.add('btn-warning');
+    } else if (forecastData.current.uvi > 6) {
+        uvButton.classList.add('btn-danger');
     }
-
-// Set user's position
-function setPosition(position){
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-
-    getWeather(latitude, longitude);
 }
 
-// Show error if issue with Geolocation
-function showError(error){
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+function addSearchedButton(city) {
+    var historyButton = document.createElement('button');
+    historyButton.classList.add('btn', 'btn-info');
+    historyButton.setAttribute('type', 'button');
+    historyButton.textContent = city;
+    historyButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        getCurrentWeather(city);
+    });
+    searchHistoryEl.appendChild(historyButton);
 }
 
-// Get weather from API
-function getWeather(latitude, longitude){
-    var api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${key}`;
-
-    fetch(api)
-        .then(function(response){
-            var data = response.json();
-            return data;
-        })
-        .then(function(data){
-            weather.temperature.value = Math.floor(data.main.temp);
-            weather.description = data.weather[0].description;
-            weather.icon = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country; 
-        })
-        .then(function(){
-            displayWeather();
-        });
-}        
-
-function displayWeather(){
-    iconElement.innerHTML = `<img src="./assets/images/icons/${weather.icon}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span>F</span>`;
-    descElement.innerHTML = weather.description;
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+function handlehistoricSearches(currSearch) {
+    searchHistory = localStorage.getItem('searchHistory')
+    if (!searchHistory) {
+        let searchArray = [];
+        localStorage.setItem("searchHistory", JSON.stringify(searchArray));
+    }
+    if (!currSearch) {
+        return JSON.parse(localStorage.getItem('searchHistory'));
+    } else {
+        let searchObj = JSON.parse(localStorage.getItem('searchHistory'))
+        let searchArray = [...searchObj];
+        searchArray.push(currSearch);
+        let distinct = [... new Set(searchArray)];
+        localStorage.setItem("searchHistory", JSON.stringify(distinct));
+        localStorage.setItem("lastCity", currSearch);
+    }
+    populatePastButtons()
 }
+
+function populatePastButtons() {
+    const searchedArray = handlehistoricSearches();
+    searchHistoryEl.innerHTML = '';
+    searchedArray.forEach(city => {
+        addSearchedButton(city);
+    });
+}
+
+//------------------AJAX Calls------------------//
+
+function getFiveDayForecast() {
+    let baseUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+    baseUrl = baseUrl.replace('{lat}', currCity.lat);
+    baseUrl = baseUrl.replace('{lon}', currCity.lon);
+    baseUrl = baseUrl.replace('{part}', 'minutely,hourly,alerts');
+    baseUrl = baseUrl.replace('{API key}', apiKey);
+    baseUrl += '&units=imperial'
+    $.ajax({
+        url: baseUrl,
+        type: "GET",
+        dataType: 'json'
+    }).done ((data) => {
+        forecastData = data;
+        updateFiveDayForecast();
+    }).fail (() => {
+        console.log("Something went wrong with the call for the future forecast");
+    })
+}
+
+function getCurrentWeather(city) {
+    let baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
+    baseUrl = baseUrl.replace('{city name}', city);
+    baseUrl = baseUrl.replace('{API key}', apiKey);
+    baseUrl += '&units=imperial'
+    $.ajax({
+        url: baseUrl,
+        type: "GET",
+        dataType: 'json'
+    }).done ((data) => {
+        weatherData = data;
+        updateCurrentWeatherCard(city)
+    }).fail (() => {
+        console.log("Something went wrong with the call for current weather");
+    })
+}
+//------------------End AJAX Calls------------------//
+
+//------------------Event Listeners------------------//
+searchBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    const citySearched = document.querySelector("#city-input").value.trim();
+    if (!citySearched) {
+        return;
+    }
+    getCurrentWeather(citySearched);
+});
